20728a6783d85140a4e99550fd1294d73ec29ea1
Óskar Helgi <16443844+oscar6662@users.noreply.github.com>
2023-03-28 00:25:39 +0200

    createChild button added

--- app/src/main/java/is/hi/hbv601g/petraapp/ParentActivity.kt
+++ app/src/main/java/is/hi/hbv601g/petraapp/ParentActivity.kt
@@ -1,15 +1,25 @@
 package `is`.hi.hbv601g.petraapp
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import `is`.hi.hbv601g.petraapp.Entities.Child
+import `is`.hi.hbv601g.petraapp.Entities.DaycareWorker
 import `is`.hi.hbv601g.petraapp.adapters.ChildAdapterParent
+import `is`.hi.hbv601g.petraapp.adapters.DaycareWorkerCardAdapter
 
 class ParentActivity : AppCompatActivity() {
     private lateinit var ChildList: ArrayList<Child>
+    private lateinit var mCreateNewChild: Button
+
     companion object {
         const val TAG: String = "ParentActivity"
     }
@@ -32,6 +42,11 @@ class ParentActivity : AppCompatActivity() {
                 ssn = "5552300"
             )
             );
+
+        mCreateNewChild = findViewById(R.id.newChildButton)
+        mCreateNewChild.setOnClickListener {
+            Toast.makeText(this@ParentActivity, "Create new child!", Toast.LENGTH_SHORT).show()
+        }
 
         val adapter = ChildAdapterParent(ChildList,this);
 
